9d805a9eaee62e4a8f3b14bc8a8b57d06d5538c9
archinit <archit.7537@gmail.com>
2026-01-23 17:20:27 +0530

    updated: chat-room-page

--- app/room/[roomId]/page.tsx
+++ app/room/[roomId]/page.tsx
@@ -1,14 +1,24 @@
 "use client"
 
 import { useParams } from "next/navigation"
-import { useState } from "react";
+import { useRef, useState } from "react";
+
+function formatTimeRemaining(seconds: number){
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${mins}:${secs.toString().padStart(2, "0")}`
+}
 
 export default function ChatRoom(){
 
     const params = useParams();
     const roomId = params.roomId as string
 
-    const [copyStatus, setCopyStatus] = useState("COPY")
+    const [input, setInput] = useState("");
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const [copyStatus, setCopyStatus] = useState("COPY");
+    const [timeRemaining, setTimeRemaining] = useState<number | null>(50);
 
     const copyLink = () => {
         const url = window.location.href
@@ -32,10 +42,54 @@ export default function ChatRoom(){
                     </div>
                 </div>
 
-                <div>
-                    right here
+                <div className=" h-8 w-px bg-zinc-800"/>
+
+                <div className=" flex flex-col">
+                    <span className="text-xs text-zinc-500 uppercase">Self Destruct</span>
+                    <span className={`text-sm font-bold flex items-center gap-2 ${timeRemaining !== null && timeRemaining< 60 ? "text-red-500": "text-amber-500"}`}>
+                        {timeRemaining !== null ? formatTimeRemaining(timeRemaining) : "--:--"}
+                    </span>
                 </div>
             </div>
+
+            <button className="text-xs bg-zinc-800 hover:bg-red-600 px-3
+            py-1.5 rounded text-zinc-400 hover:text-white font-bold transition-all
+            group flex items-center gap-2 disabled:opacity-50">
+                <span className="group-hover:animate-pulse">💣</span>
+                DESTROY NOW
+            </button>
         </header>
+
+        <div className="flex-1 overflow-y-auto p-4 space-y-4 scrollbar-thin">
+            for messages
+        </div>
+
+        <div className="p-4 border-t border-zinc-800 bg-zinc-900/30">
+            <div className="flex gap-4">
+                <div className="flex-1 relative group:">
+                    <span className="absolute left-4 top-1/2 -translate-y-1/2 text-green-500 animate-pulse">
+                        {">"}
+                    </span>
+                    <input
+                    autoFocus 
+                    type="text"
+                    value={input}
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter" && input.trim()) {
+                            //TODO: send message
+                            inputRef.current?.focus()
+                        }
+                    }}
+                    placeholder="Type message..."
+                    onChange={(e) => setInput(e.target.value)} 
+                    className="w-full bg-black border border-zinc-800 focus:border-zinc-700
+                    focus:outline-none transition-colors text-zinc-100 placeholder:text-zinc-700 py-3
+                    pl-8 pr-4 text-sm" />
+                </div>
+
+                <button className="bg-zinc-800 text-zinc-400 px-6 text-sm font-bold hover:text-zinc-200 transition-all disabled:opacity-50
+                disabled:cursor-not-allowed cursor-pointer">SEND</button>
+            </div>
+        </div>
     </main>
 }
