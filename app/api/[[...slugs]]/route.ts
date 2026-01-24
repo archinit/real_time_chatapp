9cc1dc965898b8796b24643ab96afe56c265888b
archinit <archit.7537@gmail.com>
2026-01-24 17:05:42 +0530

    added: messages api endpoint with zod validation

--- app/api/[[...slugs]]/route.ts
+++ app/api/[[...slugs]]/route.ts
@@ -1,6 +1,9 @@
 import { redis } from '@/lib/redis';
 import { Elysia, t } from 'elysia'
 import { nanoid } from 'nanoid';
+import { authMiddleware } from './auth';
+import { z } from 'zod';
+
 
 const ROOM_TTL_SECONDS = 60 * 10;
 
@@ -18,7 +21,30 @@ const rooms = new Elysia({ prefix: "/room"})
     return { roomId }
 })
 
-const app = new Elysia({ prefix: '/api' }).use(rooms);
+const messages = new Elysia({ prefix: 
+    "/messages" })
+    .use(authMiddleware)
+    .post("/", async ({body, auth}) => {
+        const { sender, text } = body
+        const { roomId } = auth
+        
+        const roomExists = await redis.exists(`meta:${roomId}`)
+
+        if (!roomExists) {
+            throw new Error("Room does not exist")
+        }
+
+    }, {
+        query: z.object({
+            roomId: z.string()
+        }),
+        body: z.object({
+            sender: z.string().max(100),
+            text: z.string().max(1000),
+        })
+    })
+
+const app = new Elysia({ prefix: '/api' }).use(rooms).use(messages);
 export const GET = app.fetch 
 export const POST = app.fetch 
 
